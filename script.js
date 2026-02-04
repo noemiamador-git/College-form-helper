2c9bdb8735b3e4d20df9af1a0e34269a44ec96ae
noemiamador-git <noemiamador.work@gmail.com>
2026-02-04 12:19:18 -0600

    Add form submission logic and validation

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const form = document.getElementById("collegeForm");
+const output = dcoument.getElementById("output")
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById ("name").value;
+  const gpa = document.getElementById("gpa").value;
+  const major = document.getElementById("major").value;
+  const activities = document.getElementById("activities").value;
+
+  if (!name || !gpa || !major || !activities) {
+    output.innerHTML = "<p>Please fill out all fields.</p>";
+    return;
+  }
+
+  output.innerHTML = 
+    <h2>Application Summary </h2>
+  <p><strong>Name:</strong> ${name}</p>
+  <p><strong>GPA:</strong> ${gpa}</p>
+  <p><strong>Major:</strong> ${major}</p>
+  <p><strong>Activities:</strong> ${activities}</p>
+ ';
+});
